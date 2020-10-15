cc2510c95ef36786bd7ec431ecaf45f8375b5b16
Matthieu Tristram <tristram@lal.in2p3.fr>
2020-10-15 19:02:51 +0200

    Comment foregrounds class

--- foregrounds.py
+++ foregrounds.py
@@ -11,13 +11,17 @@ class fgmodel(object):
     '''
     Class of foreground model for the Hillipop likelihood
     Units: Dl in muK^2
+    Should return the model in Dl for a foreground emission given the parameters for all correlation of frequencies
     '''
+
+    #Planck values
     
     # MJy.sr-1 -> Kcmb for (100,143,217) GHz
     gnu = {100:244.059, 143:371.658, 217:483.485}
+    
     #frequency dependence (100,143,217,353,545,857) GHz of the SZ effect
     fnu = {100:-4.031, 143:-2.785, 217:0.187, 353:6.205, 545:14.455, 857:26.335}
-
+    
     def __init__( self, freqs, parname, filename, auto=False):
         '''
         Create model for foreground
@@ -29,7 +33,7 @@ class fgmodel(object):
     
     def compute_dl( self, pars):
         '''
-        Return spectra model for each cross-frequency
+        Return spectra model for each cross-spectra
         '''
         pass
 
