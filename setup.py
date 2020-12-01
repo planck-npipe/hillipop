f214bcadc246d2389ba65557d4141383bbea9646
Xavier Garrido <xavier.garrido@gmail.com>
2020-12-01 09:47:31 +0100

    prepare package for pypi publication

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from setuptools import find_packages, setup
 
+import versioneer
+
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
-    name="hillipop",
-    version="0.1",
+    name="planck_2020_hillipop",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description="A cobaya high-ell likelihood polarized for planck",
-    zip_safe=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Matthieu Tristram",
+    url="https://github.com/planck-npipe/lollipop",
+    license="GNU license",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     packages=find_packages(),
     python_requires=">=3.5",
     install_requires=["astropy", "cobaya>=3.0"],
-    package_data={"hillipop": ["Hillipop.yaml", "Hillipop.bibtex"]},
+    package_data={"planck_2020_hillipop": ["Hillipop.yaml", "Hillipop.bibtex"]},
 )
